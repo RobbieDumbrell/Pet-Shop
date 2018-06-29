0a52ee90729637884e15a5b7bcc988747c09f052
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-06-29 15:15:00 +0100

    tests 20, 21 and 22 passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -107,4 +107,16 @@ def customer_can_afford_pet(customer, pet)
     return false
   end
 end
-# Passed!
+# Passed! and Passed!
+
+# 20, 21 and 22.
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return if pet == nil
+  if customer_can_afford_pet(customer, pet) == true
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop, pet[:price])
+  end
+end
+# Passed! Passed! and Passed!
