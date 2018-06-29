582d1ae7bea49b51988d43ffec19db6e95321309
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-06-29 10:52:08 +0100

    tests 1 and 2 passed

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,13 @@
+# 1.
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+# Passed!
+
+# 2.
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+# Passed!
+
+# 3.
--- start_point/specs/pet_shop_spec.rb
+++ start_point/specs/pet_shop_spec.rb
@@ -72,21 +72,27 @@ class TestPetShop < Minitest::Test
     }
   end
 
-  def test_pet_shop_name
-    name = pet_shop_name(@pet_shop)
-    assert_equal("Camelot of Pets", name)
-  end
 
+# 1.
+  # def test_pet_shop_name
+  #   name = pet_shop_name(@pet_shop)
+  #   assert_equal("Camelot of Pets", name)
+  # end
+
+
+# 2.
   # def test_total_cash
   #   sum = total_cash(@pet_shop)
   #   assert_equal(1000, sum)
   # end
 
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(1010, cash)
-  # end
+
+# 3.
+  def test_add_or_remove_cash__add
+    add_or_remove_cash(@pet_shop,10)
+    cash = total_cash(@pet_shop)
+    assert_equal(1010, cash)
+  end
 
   # def test_add_or_remove_cash__remove
   #   add_or_remove_cash(@pet_shop,-10)
